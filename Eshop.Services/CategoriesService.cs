b5526ae314750d8bbc77765bfd781d75ada1eb8f
Toheeb Azeez <toyyibazeez@gmail.com>
2021-02-03 21:42:55 +0100

    CRUD operations with EF for Category

---
+++ Eshop.Services/CategoriesService.cs
@@ -0,0 +1,57 @@
+﻿using Eshop.Database;
+using Eshop.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eshop.Services
+{
+    public class CategoriesService
+    {
+        public void SaveCategory(Category category)
+        {
+            using(var context = new EshopDBContext())
+            {
+                context.Categories.Add(category);
+                context.SaveChanges();
+            }
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            using (var context = new EshopDBContext())
+            {
+                context.Entry(category).State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        public List<Category> GetCategories()
+        {
+            using (var context = new EshopDBContext())
+            {
+                return context.Categories.ToList();
+            }
+        }
+
+        public Category GetCategory(int id)
+        {
+            using (var context = new EshopDBContext())
+            {
+                return context.Categories.Find(id);
+            }
+        }
+
+        public void DeleteCategory(Category category)
+        {
+            using (var context = new EshopDBContext())
+            {
+                context.Categories.Remove(category);
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Eshop.Web/Controllers/CategoryController.cs
@@ -0,0 +1,64 @@
+﻿using Eshop.Entities;
+using Eshop.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Eshop.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoriesService categoryService = new CategoriesService();
+        // GET: Category
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var categories = categoryService.GetCategories();
+            return View(categories);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            categoryService.SaveCategory(category);
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int ID)
+        {
+            var category = categoryService.GetCategory(ID);
+            return View(category);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Category category)
+        {
+            categoryService.UpdateCategory(category);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int CategoryID)
+        {
+            var category = categoryService.GetCategory(CategoryID);
+            return View(category);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Category category)
+        {
+            category = categoryService.GetCategory(category.ID);
+            categoryService.DeleteCategory(category);
+            return RedirectToAction("Index");
+        }
+    }
+}
