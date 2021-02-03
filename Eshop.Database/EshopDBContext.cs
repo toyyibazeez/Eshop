a6ec8ad676a570b9093bfea4a5b6c840da95b6e6
Toheeb Azeez <toyyibazeez@gmail.com>
2021-02-03 11:43:27 +0100

    Added Projects for Web, Products and Catgories Entities and Migrations

---
+++ Eshop.Database/EshopDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Eshop.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eshop.Database
+{
+    public class EshopDBContext : DbContext
+    {
+        public EshopDBContext() : base("EshopConnection")
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ Eshop.Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eshop.Entities
+{
+    public class Product : BaseEntity
+    {
+        public Category Category { get; set; }
+        public decimal Price { get; set; }
+    }
+}
