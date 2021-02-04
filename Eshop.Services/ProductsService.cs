fadd5a087b86a4dcb518e3e07f67cab28a6d1881
Toheeb Azeez <toyyibazeez@gmail.com>
2021-02-04 22:51:34 +0100

    Product Servicves and CRUD Actions

---
+++ Eshop.Services/ProductsService.cs
@@ -0,0 +1,57 @@
+﻿using Eshop.Database;
+using Eshop.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eshop.Services
+{
+    public class ProductsService
+    {
+        public void SaveProduct(Product product)
+        {
+            using(var context = new EshopDBContext())
+            {
+                context.Products.Add(product);
+                context.SaveChanges();
+            }
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            using (var context = new EshopDBContext())
+            {
+                context.Entry(product).State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        public List<Product> GetProducts()
+        {
+            using (var context = new EshopDBContext())
+            {
+                return context.Products.ToList();
+            }
+        }
+
+        public Product GetProduct(int id)
+        {
+            using (var context = new EshopDBContext())
+            {
+                return context.Products.Find(id);
+            }
+        }
+
+        public void DeleteProduct(Product product)
+        {
+            using (var context = new EshopDBContext())
+            {
+                context.Products.Remove(product);
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Eshop.Web/Controllers/ProductController.cs
@@ -0,0 +1,64 @@
+﻿using Eshop.Entities;
+using Eshop.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Eshop.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductsService productService = new ProductsService();
+        // GET: Category
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var categories = productService.GetProducts();
+            return View(categories);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            productService.SaveProduct(product);
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int ID)
+        {
+            var product = productService.GetProduct(ID);
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            productService.UpdateProduct(product);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int productID)
+        {
+            var category = productService.GetProduct(productID);
+            return View(category);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Product product)
+        {
+            product = productService.GetProduct(product.ID);
+            productService.GetProduct(product.ID);
+            return RedirectToAction("Index");
+        }
+    }
+}
